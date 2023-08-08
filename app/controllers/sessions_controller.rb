693af4e00f0139983a1ea3a8d57060d66004b15c
nekonekko <momoichigodayo+github@gmail.com>
2023-08-08 21:27:40 +0900

    feat: :sparkles: display login failure message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # redirect after login
+    else
+      flash[:danger] = "Invalid email/passwprd combination"
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
 end
