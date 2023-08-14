45f5eeea14d2bb0121a3bd66b7b699d11a9abad3
nekonekko <momoichigodayo+github@gmail.com>
2023-08-14 14:07:44 +0900

    feat: :passport_control: require login to edit/update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -26,7 +28,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      # 更新に成功した場合を扱う
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit', status: :unprocessable_entity
     end
@@ -41,5 +44,12 @@ class UsersController < ApplicationController
       :password,
       :password_confirmation
     )
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url, status: :see_other
+    end
   end
 end
