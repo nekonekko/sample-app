5ea751c0bb2a1b83d63a313211062b26416f4170
nekonekko <momoichigodayo+github@gmail.com>
2023-08-15 17:37:30 +0900

    test: :white_check_mark: test all static pages

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  test "layout links" do
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "layout links with login" do
+    log_in_as(@user)
     get root_path
     assert_template "static_pages/home"
     assert_select "a[href=?]", root_path, count: 2
@@ -9,9 +15,27 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+    get help_path
+    assert_select "title", full_title("Help")
+    get about_path
+    assert_select "title", full_title("About")
     get contact_path
     assert_select "title", full_title("Contact")
-    get signup_path
-    assert_select "title", full_title("Sign up")
+  end
+
+  test "layout links without login" do
+    get root_path
+    assert_template "static_pages/home"
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+    get help_path
+    assert_select "title", full_title("Help")
+    get about_path
+    assert_select "title", full_title("About")
+    get contact_path
+    assert_select "title", full_title("Contact")
   end
 end
